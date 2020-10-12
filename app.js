4f41972fd482fc71ab38aa08ba77df77d5e3bdb1
amandahp <amandahoffmann96@gmail.com>
2020-10-12 18:26:09 -0300

    adicionei mais pokemon

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 const promises = [];
-for (let i = 1; i <= 150; i++) {
+for (let i = 1; i <= 600; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then(res => res.json()));
 };
-/**Promise.all(promises).then(results => {
-    console.log(results);
-});
-
-const pokemon = results.map(data => ({
-    name: data.name,
-    id: data.id,
-    image: data.sprites["front_default"],
-    type: data.types.map(type => type.type.name).join(", "),
-    })
-);*/
-
-//const displayPokemon = pokemon => {};
-
-
-
-/**const pokemonHTMLString = pokemon
-    .map(
-    pokeman =>
-    ` <li class="card"> <img class="card-image" src="${pokeman.image}"/> <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2> <p class="card-subtitle">Type: ${pokeman.type}</p> </li> `
-    )
-.join("");*/
 
 
 
