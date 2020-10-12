92741eb9ff20ae73f674178597fd0de41d052f22
amandahp <amandahoffmann96@gmail.com>
2020-10-12 18:11:21 -0300

    Pokedex

---
+++ app.js
@@ -0,0 +1,55 @@
+const promises = [];
+for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then(res => res.json()));
+};
+/**Promise.all(promises).then(results => {
+    console.log(results);
+});
+
+const pokemon = results.map(data => ({
+    name: data.name,
+    id: data.id,
+    image: data.sprites["front_default"],
+    type: data.types.map(type => type.type.name).join(", "),
+    })
+);*/
+
+//const displayPokemon = pokemon => {};
+
+
+
+/**const pokemonHTMLString = pokemon
+    .map(
+    pokeman =>
+    ` <li class="card"> <img class="card-image" src="${pokeman.image}"/> <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2> <p class="card-subtitle">Type: ${pokeman.type}</p> </li> `
+    )
+.join("");*/
+
+
+
+const displayPokemon = pokemon => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+    .map(
+        pokeman =>
+        ` <li class="card"> <img class="card-image" src="${pokeman.image}"/> 
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2> <p class="card-subtitle"><span class="tituloTipo">TIPO</span><p><span class="type1"> ${pokeman.type} </span></p></p> </li> `
+    )
+    .join("");
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+
+
+Promise.all(promises).then(results => {
+    const pokemon = results.map(data => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites["front_default"],
+        type: data.types.map(type => type.type.name).join(" " + "\n"),
+    }));
+    displayPokemon(pokemon);
+});
+const pokedex = document.getElementById("pokedex");
+pokedex.innerHTML = pokemonHTMLString;
